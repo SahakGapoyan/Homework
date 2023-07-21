f74698fb81cf6a5c73c5e25dc9e364fe96becaa1
Sahak Gapoyan <139626208+SahakGapoyan@users.noreply.github.com>
2023-07-21 22:30:24 +0400

    There are some issues in InsertAt and RemoveDuplicates methods. Unhandled exception. System.IndexOutOfRangeException: Index was outside the bounds of the array.

---
+++ CustomStringBuilder/ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CustomStringBuilder
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            CustomStringBuilder bed = new CustomStringBuilder("Hello");
+            CustomStringBuilder dog = new CustomStringBuilder("First");
+            Console.WriteLine(bed.Pength());
+            Console.WriteLine(dog.Pength());
+            Console.WriteLine(dog.Appeng(" Class"));
+            Console.WriteLine(bed.Appeng(" World"));
+            Console.WriteLine(bed.InsertAt("d", 3));
+            Console.WriteLine(bed.RemoveDuplicates());
+        }
+    }
+}
+
